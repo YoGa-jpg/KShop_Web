6a3c7648a9581e2c508827ede6bf317c91eab595
YoGa-jpg <antondavidovich45@gmail.com>
2021-07-06 22:55:09 +0300

    fixed: gnrl

--- js/index.jsx
+++ js/index.jsx
@@ -15,8 +15,64 @@ class CategoryButton extends React.Component {
         return <a style={btnStyle} class="generalbutton" href="#" onClick={() => this.press(18, 'afaf')}>{this.props.name}</a>;
     }
 }
-const elements = [<CategoryButton name="Telefony"/>, <CategoryButton name="Komputery"/>,
-    <CategoryButton name="Laptopy"/>, <CategoryButton name="Сzęści komputera"/>];
+
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return <div class="container">
+        <div class="card">
+            <div class="imgBx">
+                <img src="https://lmt.mstatic.lv/devices/images/700x980/Apple_iPhone_11_Pro_64GB90442.png"/>
+            </div>
+            <div class="contentBx">
+                <h2>Apple iPhone 11 Pro</h2>
+                <div class="size">
+                    <h3>Apple iOS, 5,8" ekran OLED (1125x2436), Apple A13 Bionic, 4 GB RAM, 64 GB pamięci flash, aparat 12 MP, bateria 3190 mAh, 1 karta SIM</h3>
+                </div>
+                <a href="#">Otwarty</a>
+            </div>
+        </div>
+    </div>
+    }
+}
+
+class Good{
+    constructor(image, name, desc) {
+        this.image = image;
+        this.name = name;
+        this.desc = desc;
+    }
+}
+
+const elements = [
+    <CategoryButton name="Telefony"/>,
+    <CategoryButton name="Komputery"/>,
+    <CategoryButton name="Laptopy"/>, 
+    <CategoryButton name="Сzęści komputera"/>
+];
+
+// const goods = [
+//     new Good('https://lmt.mstatic.lv/devices/images/700x980/Apple_iPhone_11_Pro_64GB90442.png', 'Apple iPhone 11 Pro', 'Apple iOS, 5,8" ekran OLED (1125x2436), Apple A13 Bionic, 4 GB RAM, 64 GB pamięci flash, aparat 12 MP, bateria 3190 mAh, 1 karta SIM'),
+//     new Good('https://lmt.mstatic.lv/devices/images/700x980/Apple_iPhone_11_Pro_64GB90442.png', 'Apple iPhone 11 Pro', 'Apple iOS, 5,8" ekran OLED (1125x2436), Apple A13 Bionic, 4 GB RAM, 64 GB pamięci flash, aparat 12 MP, bateria 3190 mAh, 1 karta SIM'),
+//     new Good('https://lmt.mstatic.lv/devices/images/700x980/Apple_iPhone_11_Pro_64GB90442.png', 'Apple iPhone 11 Pro', 'Apple iOS, 5,8" ekran OLED (1125x2436), Apple A13 Bionic, 4 GB RAM, 64 GB pamięci flash, aparat 12 MP, bateria 3190 mAh, 1 karta SIM'),
+//     new Good('https://lmt.mstatic.lv/devices/images/700x980/Apple_iPhone_11_Pro_64GB90442.png', 'Apple iPhone 11 Pro', 'Apple iOS, 5,8" ekran OLED (1125x2436), Apple A13 Bionic, 4 GB RAM, 64 GB pamięci flash, aparat 12 MP, bateria 3190 mAh, 1 karta SIM'),
+//     new Good('https://lmt.mstatic.lv/devices/images/700x980/Apple_iPhone_11_Pro_64GB90442.png', 'Apple iPhone 11 Pro', 'Apple iOS, 5,8" ekran OLED (1125x2436), Apple A13 Bionic, 4 GB RAM, 64 GB pamięci flash, aparat 12 MP, bateria 3190 mAh, 1 karta SIM'),
+// ]
+const goods = [
+    <Card />
+]
+
+function Initialize(){
+    for(var i = 0; i < goods.length; i++)
+    {
+        ReactDOM.render(
+            goods[i],
+            getElementById("good1"),
+        )
+    }
+}
 
 ReactDOM.render(
     elements[0],
@@ -37,3 +93,40 @@ ReactDOM.render(
     elements[3],
     document.getElementById("btnContainer4")
 );
+
+ReactDOM.render(
+    goods[0],
+    document.getElementById("good1")
+)
+
+ReactDOM.render(
+    goods[0],
+    document.getElementById("good2")
+)
+
+ReactDOM.render(
+    goods[0],
+    document.getElementById("good3")
+)
+
+ReactDOM.render(
+    goods[0],
+    document.getElementById("good4")
+)
+
+ReactDOM.render(
+    goods[0],
+    document.getElementById("good5")
+)
+
+ReactDOM.render(
+    goods[0],
+    document.getElementById("good6")
+)
+
+ReactDOM.render(
+    goods[0],
+    document.getElementById("good7")
+)
+
+Initialize();
